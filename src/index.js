fc5adca9d265023cf14ae6e5b65ceab51b2c00ae
SDen4 <asw325@yandex.ru>
2020-09-11 22:58:36 +0500

    template

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import "./styles/sass/main.sass";
-import Vuelidate from 'vuelidate';
 
 window.Vue = require("vue");
 
-Vue.use(Vuelidate);
 
 Vue.component("main-component", require("./components/Main.vue").default)
 
