c29ba8c5f16d6669656c5777da5830afa9f3b72e
SDen4 <asw325@yandex.ru>
2020-09-12 17:35:35 +0500

    counting rounds + unactive start button

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,19 +1,27 @@
 <template>
-    <div>
-        <div class="game">
-            <div class="game__wrapper">
-                <div class="game__segment game__segment_1" :class="{active: isActive === 1}" @click="buttonClick(1)">1</div>
-                <div class="game__segment game__segment_2" :class="{active: isActive === 2}" @click="buttonClick(2)">2</div>
-                <div class="game__segment game__segment_3" :class="{active: isActive === 3}" @click="buttonClick(3)">3</div>
-                <div class="game__segment game__segment_4" :class="{active: isActive === 4}" @click="buttonClick(4)">4</div>
+    <div class="game__container">
+        <div class="game__container_left">
+            <div class="game">
+                <div class="game__wrapper">
+                    <div class="game__segment game__segment_1" :class="{active: isActive === 1}" @click="buttonClick(1)">1</div>
+                    <div class="game__segment game__segment_2" :class="{active: isActive === 2}" @click="buttonClick(2)">2</div>
+                    <div class="game__segment game__segment_3" :class="{active: isActive === 3}" @click="buttonClick(3)">3</div>
+                    <div class="game__segment game__segment_4" :class="{active: isActive === 4}" @click="buttonClick(4)">4</div>
+                </div>
+            </div>
+            <div class="game__start_wrapper">
+                <button class="button button__start" :class="{button__start_unactive: playing}" @click="start">Старт</button>
             </div>
         </div>
-        <button class="game__start" @click="start">Start</button>
+        <info-component :arrayOfAi="arrayOfAi"></info-component>
     </div>
 </template>
 
 <script>
     export default {
+        components: {
+            infoComponent: ()=> import('./Info.vue'),
+        },
         data() {
             return {
                 durationAI: 500, //duration of steps
@@ -22,7 +30,8 @@
                 arrayOfPlayer: [], //user's steps
                 arrayOfAi: [], //AI's steps
                 aiPlay: true, //flag of players: user and AI
-                isActive: 0 //index if active button
+                isActive: 0, //index if active button
+                playing: false
             }
         },
         methods: {
@@ -41,6 +50,8 @@
                 }
             },
             start() {
+                if(this.playing) return;
+                this.playing = true;
                 this.handleAI();
             },
             handleAI() {
---
+++ src/components/Info.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="info">
+        <div class="info__wrapper">
+            <div class="info__rounds">Раунд: {{arrayOfAi.length == 0 ? "" : arrayOfAi.length}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            arrayOfAi: Array
+        },
+        data() {
+            return {
+                rounds: 1
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
